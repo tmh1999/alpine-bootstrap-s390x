5ada04eb8dee776ee2eaed08b728a522373b5627
tmh1999 <tmhoang1999@gmail.com>
2016-07-26 14:16:39 +0700

    Result: http://lists.alpinelinux.org/alpine-devel/5425.html

--- bootstrap/createcross-toolchain.sh
+++ bootstrap/createcross-toolchain.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 set -x
-export TARGET_ARCH=armv7
+
+export TARGET_ARCH=s390x
 export ABUILD_CREATECROSS_CONF=$PWD/abuild-createcross-$TARGET_ARCH.conf
 export ABUILD_CROSSBUILD_CONF=$PWD/abuild-crossbuild-$TARGET_ARCH.conf
-export APORTS=$HOME/aports
+export APORTS=$HOME/s390x/aports
 
 CBUILDROOT="$(source $ABUILD_CREATECROSS_CONF ; echo $CBUILDROOT)"
 CTARGET="$(source $ABUILD_CREATECROSS_CONF ; echo $CTARGET)"
@@ -26,7 +27,8 @@ ABUILD_CONF=$ABUILD_CREATECROSS_CONF abuild -r || return 1
 abuild-apk --repository $REPODEST_HOST/main add binutils-$CTARGET || return 1
 
 cd $APORTS/main/musl
-ABUILD_CONF=$ABUILD_CROSSBUILD_CONF CARCH=$TARGET_ARCH abuild up2date
+# CARCH=$TARGET_ARCH
+ABUILD_CONF=$ABUILD_CROSSBUILD_CONF abuild up2date
 if [ $? -ne 0 ]; then
 	# Step 2. Install kernel headers for target
 	# Step 3. Pass1 GCC for C-headers (--with-newlib --without-headers)
@@ -36,27 +38,35 @@ if [ $? -ne 0 ]; then
 	# Step 4. C-library headers for target
 	#
 	cd $APORTS/main/musl
-	ABUILD_CONF=$ABUILD_CROSSBUILD_CONF CARCH=$TARGET_ARCH abuild unpack prepare install_sysroot_headers || return 1
+	# CARCH=$TARGET_ARCH
+	ABUILD_CONF=$ABUILD_CROSSBUILD_CONF abuild unpack prepare install_sysroot_headers || return 1
 
 	# Step 5. Pass2 GCC for C-library (--with-newlib --enable-threads=no --disable-bootstrap)
 	#
 	cd $APORTS/main/gcc
-	ABUILD_CONF=$ABUILD_CREATECROSS_CONF BOOTSTRAP=nolibc CTARGET_ARCH=$TARGET_ARCH CTARGET_LIBC=musl abuild -r || return 1
+	# CTARGET_ARCH=$TARGET_ARCH
+	ABUILD_CONF=$ABUILD_CREATECROSS_CONF BOOTSTRAP=nolibc CTARGET_LIBC=musl abuild -r || return 1
 
 	# Step 6. C-library
 	abuild-apk --repository $REPODEST_HOST/main add gcc-pass2-$CTARGET || return 1
 	cd $APORTS/main/musl
-	ABUILD_CONF=$ABUILD_CROSSBUILD_CONF BOOTSTRAP=noutils CTARGET_LIBC=musl abuild || return 1
+	# tmh  CARCH=$TARGET_ARCH if we add this, cant sign the index
+	# CTARGET_LIBC=musl
+	ABUILD_CONF=$ABUILD_CROSSBUILD_CONF BOOTSTRAP=noutils abuild || return 1
 	abuild-apk del gcc-pass2-$CTARGET
 fi
 
 # Step 7. Final GCC for target
 sudo mkdir -p "$CBUILDROOT/etc/apk/keys"
 sudo cp -a /etc/apk/keys/* "$CBUILDROOT/etc/apk/keys"
+#cd $REPODEST_TARGET/main
+#ln -s unknown $TARGET_ARCH
+#cd -
 abuild-apk --initdb --repository "$REPODEST_TARGET/main" --root "$CBUILDROOT" --arch "$TARGET_ARCH" add musl-dev || return 1
 
 cd $APORTS/main/gcc
-ABUILD_CONF=$ABUILD_CREATECROSS_CONF CTARGET_ARCH=$TARGET_ARCH CTARGET_LIBC=musl abuild -r || return 1
+# CTARGET_ARCH=$TARGET_ARCH 
+ABUILD_CONF=$ABUILD_CREATECROSS_CONF CTARGET_LIBC=musl abuild -r || return 1
 
 # Step 8. Clean abuild mess.
 mv $REPODEST_HOST/main/*/lib*apk $REPODEST_TARGET/main/$TARGET_ARCH
--- bootstrap/crossbuild-alpine-bootstrap.sh
+++ bootstrap/crossbuild-alpine-bootstrap.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-TARGET_ARCH=aarch64
+set -x
+TARGET_ARCH=s390x
 
 export ABUILD_CREATECROSS_CONF=$PWD/abuild-createcross-$TARGET_ARCH.conf
 export ABUILD_CONF=$PWD/abuild-crossbuild-$TARGET_ARCH.conf
-export APORTS=$HOME/aports
+export APORTS=$HOME/s390x/aports
 
 SUDO_APK=abuild-apk
 CBUILDROOT="$(source $ABUILD_CONF ; echo $CBUILDROOT)"
@@ -33,27 +34,43 @@ ${SUDO_APK} --quiet --arch "$TARGET_ARCH" --root "$CBUILDROOT" --initdb add
 ${SUDO_APK} --root "$CBUILDROOT" add libgcc libstdc++ #libgnat
 
 # optional parts
-KERNEL_PKG="linux-firmware linux-vanilla"
+#KERNEL_PKG="linux-firmware linux-vanilla"
 #DEBUG_PKG="ncurses gdb"
 
 # ordered cross-build
-for PKG in fortify-headers linux-headers musl libc-dev \
-	   busybox busybox-initscripts binutils make pkgconf \
-	   zlib openssl libfetch apk-tools \
-	   gmp mpfr3 mpc1 isl cloog gcc \
-	   openrc alpine-conf alpine-baselayout alpine-keys alpine-base build-base \
-	   attr libcap patch sudo acl fakeroot tar \
-	   pax-utils abuild openssh \
-	   ncurses util-linux lvm2 popt xz cryptsetup kmod lddtree mkinitfs \
-	   $KERNEL_PKG $DEBUG_PKG ; do
+for PKG in linux-headers musl libc-dev fortify-headers \
+		busybox busybox-initscripts binutils make pkgconf pkgconfig \
+		zlib openssl libfetch apk-tools \
+		gmp mpfr3 mpc1 isl cloog gcc \
+		openrc alpine-conf alpine-baselayout alpine-keys alpine-base build-base \
+		attr libcap patch sudo acl fakeroot tar \
+		pax-utils abuild openssh \
+		ncurses util-linux lvm2 popt xz cryptsetup kmod lddtree mkinitfs \
+		$KERNEL_PKG $DEBUG_PKG ; do
 
 	cd $APORTS/main/$PKG
+	case "$PKG" in
+	binutils)
+		cp APKBUILD APKBUILD.origin
+		cp APKBUILD.no.gold APKBUILD
+		;;
+	gcc)
+		cp APKBUILD APKBUILD.origin
+		cp APKBUILD.no.paxmark APKBUILD
+	esac
+
 	BOOTSTRAP=bootimage abuild -r || exit 1
 
 	case "$PKG" in
-	fortify-headers | libc-dev)
+	binutils | gcc)
+		cp APKBUILD.origin APKBUILD
+		;;
+	# fortify-headers | libc-dev)
+	libc-dev)
 		# install libc-dev, as implicit but mandatory dependency
 		${SUDO_APK} -u --root "$CBUILDROOT" add libc-dev || exit 1
+		# have to explicitly specify the package full path name like this
+		${SUDO_APK} -u --root "$CBUILDROOT" add "$REPODEST"/main/"$TARGET_ARCH"/musl-1.1.15-r0.apk
 		;;
 	esac
 done
